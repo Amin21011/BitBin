9f732fbda2aacb50ca23140ad0b9302a4c8a77cd
Niek Goedemans <niekgoedemans93@gmail.com>
2022-05-30 14:35:47 +0200

    gg

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="navBar">
+        <div class="home">
+        <a href="#Home">HOME</a>
+        </div>
+
+        <div class="tools">
+        <a href="#Tools">TOOLS</a>
+        </div>
+
+        <div class="login">
+        <a href="#Login">LOGIN</a>
+        </div>
+
+        <div class="signUp">
+        <a href="#SignUp">SIGN UP</a>
+        </div>
+    </div>
+</body>
+</html>
