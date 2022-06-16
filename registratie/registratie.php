f5e3e18ae2f0ad8904bc275030de6d0f04e88fa1
ivano_baptista <Iano.baptista@hotmail.com>
2022-06-16 11:33:49 +0200

    updates

--- registratie/registratie.php
+++ registratie/registratie.php
@@ -22,14 +22,17 @@ session_start();
 </head>
     <body>
     <nav class="navbar">
-        <a href="/phpfolder/Verdieping-f85d899f-d4b19a2a/Homepage/homepage.html" class="logo">
+
+        <a href="/Verdieping-f85d899f-d4b19a2a/Homepage/homepage.html" class="logo">
             <h1>BitBin</h1>
         </a>
+
         <div class="nav-links">
-            <div class="nav-item"><a href="/phpfolder/Verdieping-f85d899f-d4b19a2a/Codefield/CodeField.html">Code</a></div>
-            <div class="nav-item"><a href="/phpfolder/Verdieping-f85d899f-d4b19a2a/registratie/login.php">Log in</a></div>
-            <div class="nav-item"><a href="/phpfolder/Verdieping-f85d899f-d4b19a2a/registratie/registratie.php">Sign up</a></div>
+            <div class="nav-item"><a href="/Verdieping-f85d899f-d4b19a2a/Codefield/CodeField.html">Code</a></div>
+            <div class="nav-item"><a href="/Verdieping-f85d899f-d4b19a2a/registratie/login.php">Log in</a></div>
+            <div class="nav-item"><a href="/Verdieping-f85d899f-d4b19a2a/registratie/registratie.php">Sign up</a></div>
         </div>
+    </nav>
     </nav>
         <form method="post">
             <table>
