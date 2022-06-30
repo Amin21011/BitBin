76a5f555a55922b52cc66ebbe0fee7bdfbf0d5f1
Niek Goedemans <niekgoedemans93@gmail.com>
2022-06-30 10:37:11 +0200

    gg

--- SeeCode.php
+++ SeeCode.php
@@ -2,66 +2,60 @@
 
 include 'conn.php';
 
-$uniqueLink = uniqid();
+$uniqueLink = 
+uniqid();
 $editor = $_POST["editor"];
 $language = $_POST["Code-Highlighting-Select"];
 
-$sql1 ="INSERT INTO codedata (code, language, uniqueLink) 
+$sql1 = "INSERT INTO codedata (code, language, uniqueLink) 
 VALUES ('$editor', '$language', '$uniqueLink')";
 $stmt1 = $pdo->prepare($sql1);
 $stmt1->execute();
 
 $sql2 = "SELECT * FROM codedata";
-$codeResult = $sql2;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>BitbinShare</title>
-        <!-- Styling of the page -->
+    <head>
+        <title>BitbinShare</title>
+            <!-- Styling of the page -->
     
-    <link rel="stylesheet" type="text/css" href="CodeMirror-5.65.4/lib/codemirror.css">
-        <!-- Scripts for the page -->
-    <script src="CodeMirror-5.65.4/lib/codemirror.js"></script>
-    <script src="CodeMirror-5.65.4/mode/xml/xml.js"></script>
-    <script src="CodeMirror-5.65.4/mode/css/css.js"></script>
-    <script src="CodeMirror-5.65.4/mode/javascript/javascript.js"></script>
-    <script src="CodeMirror-5.65.4/mode/php/php.js"></script>
-    <script src="CodeMirror-5.65.4/mode/python/python.js"></script>
-    <script src="CodeMirror-5.65.4/mode/clike/clike.js"></script>
-    <script src="CodeMirror-5.65.4/mode/swift/swift.js"></script>
-    <script src="CodeMirror-5.65.4/mode/sql/sql.js"></script>
-        <!-- Addons for the page -->
-    <script src="CodeMirror-5.65.4/addon/edit/closetag.js"></script>
-    <script src="CodeMirror-5.65.4/addon/edit/matchbrackets.js"></script>
-        <!-- Css for the textarea-->
-    <link href="CodeMirror-5.65.4/theme/dracula.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="Getcode.css">
-</head>
-        <body>
-            <nav class="navbar">
-                <a href="index.html" class="logo">
-                    <h1>BitBin</h1>
-                </a>
+        <link rel="stylesheet" type="text/css" href="CodeMirror-5.65.4/lib/codemirror.css">
+            <!-- Scripts for the page -->
+        <script src="CodeMirror-5.65.4/lib/codemirror.js"></script>
+        <script src="CodeMirror-5.65.4/mode/xml/xml.js"></script>
+        <script src="CodeMirror-5.65.4/mode/css/css.js"></script>
+        <script src="CodeMirror-5.65.4/mode/javascript/javascript.js"></script>
+        <script src="CodeMirror-5.65.4/mode/php/php.js"></script>
+        <script src="CodeMirror-5.65.4/mode/python/python.js"></script>
+        <script src="CodeMirror-5.65.4/mode/clike/clike.js"></script>
+        <script src="CodeMirror-5.65.4/mode/swift/swift.js"></script>
+        <script src="CodeMirror-5.65.4/mode/sql/sql.js"></script>
+            <!-- Addons for the page -->
+        <script src="CodeMirror-5.65.4/addon/edit/closetag.js"></script>
+        <script src="CodeMirror-5.65.4/addon/edit/matchbrackets.js"></script>
+            <!-- Css for the textarea-->
+        <link href="CodeMirror-5.65.4/theme/dracula.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="Getcode.css">
+    </head>
+    <body>
+        <nav class="navbar">
+            <a href="index.html" class="logo">
+                <h1>BitBin</h1>
+            </a>
 
-                <div class="nav-links">
-                    <div class="nav-item">
-                        <a href="CodeField.php">Code</a>
-                    </div>
+            <div class="nav-links">
+                <div class="nav-item">
+                    <a href="CodeField.php">Code</a>
                 </div>
-            </nav>
-            <?php
-                $lang = $_POST["Code-Highlighting-Select"];
-                $data = $_POST["editor"];
-            ?>
-            <h1>Here is your code!</h1>
-            <textarea name="codeView" id="codeView" readonly><?php 
-                if(isset($data)) {
-                    echo $data;
-                }
-            ?></textarea>
-        </body>
+            </div>
+        </nav>
+        <h1>Here is your code!</h1>
+        <textarea name="codeView" id="codeView" readonly><?php 
+        var_dump($sql2["code"]);
+        ?></textarea>
+    </body>
     <script>
         const editor = CodeMirror.fromTextArea(document.getElementById("codeView"), {
             lineNumbers: true,
