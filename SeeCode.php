40bbfc48e9c1174837d0f807f7fef47599dd70ad
Niek Goedemans <niekgoedemans93@gmail.com>
2022-06-30 08:57:45 +0200

    gg

--- SeeCode.php
+++ SeeCode.php
@@ -6,12 +6,12 @@ $uniqueLink = uniqid();
 $editor = $_POST["editor"];
 $language = $_POST["Code-Highlighting-Select"];
 
-$sql ="INSERT INTO codedata (code, language, uniqueLink) 
+$sql1 ="INSERT INTO codedata (code, language, uniqueLink) 
 VALUES ('$editor', '$language', '$uniqueLink')";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
+$stmt1 = $pdo->prepare($sql1);
+$stmt1->execute();
 
-$sql2 = "SELECT code FROM codedata";
+$sql2 = "SELECT * FROM codedata";
 $codeResult = $sql2;
 ?>
 
